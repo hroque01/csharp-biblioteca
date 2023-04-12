100550acebd7563d8300dec4873fc05b3b11c500
Hadson Roque <nineonezero.light@gmail.com>
2023-04-12 13:00:47 +0200

    Create filter

--- csharp-biblioteca/Biblioteca.cs
+++ csharp-biblioteca/Biblioteca.cs
@@ -1,17 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace csharp_biblioteca
 {
     internal class Biblioteca
     {
         private List<Documenti> Documenti = new List<Documenti>();
+
+        public Biblioteca()
+        {
+            AggiungiDocumenti();
+        }
+
+        private void AggiungiDocumenti()
+        {
+            //DvD gia esistenti
+            Dvd dvd1 = new Dvd("D01A", "Iron Man", "2005", "Azione", "D1", "Marvel", "1:45:00");
+            Dvd dvd2 = new Dvd("D02B", "Inception", "2010", "Fantascienza", "D2", "Christopher Nolan", "2:28:00");
+            Dvd dvd3 = new Dvd("D03C", "Pirati dei Caraibi: La Maledizione del forziere fantasma", "2006", "Avventura", "D3", "Gore Verbinski", "2:31:00");
+            Dvd dvd4 = new Dvd("D04D", "The Avengers", "2012", "Azione", "D4", "Marvel", "2:23:00");
+            Dvd dvd5 = new Dvd("D02B", "Jurassic Park", "1993", "Fantascienza", "B", "Steven Spielberg", "2:06:00");
+
+
+            //Libri gia esistenti
+            Libri libro1 = new Libri("L02B", "Il signore degli anelli", "1954", "Fantasy", "B", "J.R.R. Tolkien", "1216");
+            Libri libro2 = new Libri("L03C", "Il nome della rosa", "1980", "Giallo", "C", "Umberto Eco", "536");
+            Libri libro3 = new Libri("L04A", "La solitudine dei numeri primi", "2008", "Drammatico", "A", "Paolo Giordano", "296");
+            Libri libro4 = new Libri("L05B", "Le città invisibili", "1972", "Romanzo", "B", "Italo Calvino", "165");
+            Libri libro5 = new Libri("L06C", "L'isola del giorno prima", "1994", "Romanzo", "C", "Umberto Eco", "360");
+
+            Documenti.Add(dvd1);
+            Documenti.Add(dvd2);
+            Documenti.Add(dvd3);
+            Documenti.Add(dvd4);
+            Documenti.Add(dvd5);
+            Documenti.Add(libro1);
+            Documenti.Add(libro2);
+            Documenti.Add(libro3);
+            Documenti.Add(libro4);
+            Documenti.Add(libro5);
+        }
+
         private List<Utenti> Utenti = new List<Utenti>();
         private List<Prestiti> Prestiti = new List<Prestiti>();
+
+        
+
+       
 
         public Utenti CreateUser()
         {
@@ -102,10 +143,50 @@ namespace csharp_biblioteca
             
         }
 
-        public void AddPrestito(Utenti utente, Documenti documento, string dataInizio, string dataFine)
+        public void PrestitoDocumento()
         {
-            Prestiti nuovoPrestito = new Prestiti { Utenti = utente, Documenti = documento, dataInizio = dataInizio, dataFine = dataFine };
-            Prestiti.Add(nuovoPrestito);
+
+
+            Console.WriteLine("Vuoi cercare tramite codice o titolo?");
+            string ricerca = Console.ReadLine();
+            ricerca = ricerca.ToLower();
+            
+            if (ricerca == "codice")
+            {
+                Console.WriteLine("Inserisci il codice");
+                string codiceRicerca = Console.ReadLine();
+                Documenti.FindAll(Documenti => Documenti.Codice == codiceRicerca);
+
+                if (Documenti.Find(doc => doc.Codice == codiceRicerca) != null)
+                {
+                    Console.WriteLine("Prestito eseguito correttamente");
+                    // Qui puoi aggiungere il codice per gestire il prestito del documento
+                }
+                else
+                {
+                    Console.WriteLine("Documento non trovato!");
+                }
+
+
+            }
+
+            if ( ricerca == "titolo")
+            {
+                Console.WriteLine("Inserisci il titolo");
+                string titoloRicerca = Console.ReadLine();
+                Documenti.FindAll(Documenti => Documenti.Titolo == titoloRicerca);
+
+                if (Documenti.Find(doc => doc.Titolo == titoloRicerca) != null)
+                {
+                    Console.WriteLine("Prestito eseguito correttamente");
+                    // Qui puoi aggiungere il codice per gestire il prestito del documento
+                }
+                else
+                {
+                    Console.WriteLine("Documento non trovato!");
+                }
+            }
+
         }
 
 
--- csharp-biblioteca/Program.cs
+++ csharp-biblioteca/Program.cs
@@ -7,27 +7,15 @@ namespace csharp_biblioteca
     {
         static void Main(string[] args)
         {
-            //DvD gia esistenti
-            Dvd dvd1 = new Dvd("D01A", "Iron Man", "2005", "Azione", "D1", "Marvel", "1:45:00");
-            Dvd dvd2 = new Dvd("D02B", "Inception", "2010", "Fantascienza", "D2", "Christopher Nolan", "2:28:00");
-            Dvd dvd3 = new Dvd("D03C", "Pirati dei Caraibi: La Maledizione del forziere fantasma", "2006", "Avventura", "D3", "Gore Verbinski", "2:31:00");
-            Dvd dvd4 = new Dvd("D04D", "The Avengers", "2012", "Azione", "D4", "Marvel", "2:23:00");
-            Dvd dvd5 = new Dvd("D02B", "Jurassic Park", "1993", "Fantascienza", "B", "Steven Spielberg", "2:06:00");
-
-
-            //Libri gia esistenti
-            Libri libro1 = new Libri ( "L02B", "Il signore degli anelli","1954","Fantasy", "B","J.R.R. Tolkien","1216" );
-            Libri libro2 = new Libri ( "L03C", "Il nome della rosa", "1980", "Giallo", "C", "Umberto Eco", "536" );
-            Libri libro3 = new Libri("L04A", "La solitudine dei numeri primi", "2008", "Drammatico", "A", "Paolo Giordano", "296");
-            Libri libro4 = new Libri("L05B", "Le città invisibili", "1972", "Romanzo", "B", "Italo Calvino", "165");
-            Libri libro5 = new Libri("L06C", "L'isola del giorno prima", "1994", "Romanzo", "C", "Umberto Eco", "360");
 
             //Registrazione
             Biblioteca biblioteca = new Biblioteca();
-            biblioteca.CreateUser();
+            //biblioteca.CreateUser();
 
-            //Creazione di un documento
-            biblioteca.CreateDocument();
+            ////Creazione di un documento
+            //biblioteca.CreateDocument();
+
+            biblioteca.PrestitoDocumento();
             
         }
     }
