78fedb807e8e170daad626e3e30299ff753d6555
Hadson Roque <nineonezero.light@gmail.com>
2023-04-12 10:54:53 +0200

    Create class Prestiti

---
+++ csharp-biblioteca/Prestiti.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class Prestiti
+    {
+        public Utenti Utenti { get; set; }
+        public Documenti Documenti { get; set; }
+        public string dataInizio { get; set; }
+        public string dataFine { get; set; }
+    }
+}
