0530637623b102db25d98c15c8f346ff73496973
Hadson Roque <nineonezero.light@gmail.com>
2023-04-12 10:15:26 +0200

    Add project files.

---
+++ csharp-biblioteca/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace csharp_biblioteca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //public Utenti(string cognome, string nome, string email, string utentePassword, string telefono)
+
+            Console.Write("Registrati!");
+
+            //Cognome
+            Console.Write("Inserisci il tuo cognome: ");
+            string cognome = Console.ReadLine();
+
+            //Nome
+            Console.Write("Inserisci il tuo nome: ");
+            string nome = Console.ReadLine();
+
+
+            //Cognome
+            Console.Write("Inserisci il tuo email: ");
+            string email = Console.ReadLine();
+
+            //Password
+            Console.Write("Inserisci la tua password: ");
+            string utentePassword = Console.ReadLine();
+
+            //Telefono
+            Console.Write("Inserisci il tuo numero di telefono: ");
+            string telefono = Console.ReadLine();
+
+            Utenti nuovoUtente = new Utenti(cognome, nome, email, utentePassword, telefono);
+            Console.WriteLine($"Cognome: {nuovoUtente.Cognome} \n " +
+                $"Nome: {nuovoUtente.Nome} \n " +
+                $"Email: {nuovoUtente.Email} \n " +
+                $"Password: {nuovoUtente.UtentePassword} \n " +
+                $"Telefono: {nuovoUtente.Telefono}"
+                );
+
+
+        }
+    }
+}
---
+++ csharp-biblioteca/Utenti.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class Utenti
+    {
+        public string Cognome { get; set; }
+
+        public string Nome { get; set; }
+        
+        public string Email { get; set; }
+
+        public string UtentePassword { get; set; }
+
+        public string Telefono { get; set; }
+
+        public Utenti(string cognome, string nome, string email, string utentePassword, string telefono)
+        {
+            Cognome = cognome;
+            Nome = nome;
+            Email = email;
+            UtentePassword = utentePassword;
+            Telefono = telefono;
+        }
+    }
+}
